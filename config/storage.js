3a81b40bd6c0a1bcefc1733c3db5018d2372efaf
Gary Smith <scopevale@gmail.com>
2012-11-15 15:07:56 -0500

    set up redis config - all working!!

--- config/storage.js
+++ config/storage.js
@@ -12,9 +12,12 @@ module.exports = {
 		// redis configuration
 		//---------------------------
 		'redis' : {
-			port: 1216,
-			host: '127.0.0.1',
-			db: 1
+//			port: 1216,
+//			host: '127.0.0.1',
+//			db: 1            
+            port: 9385,
+            host: 'grouper.redistogo.com',
+            password: 'ae36c0a6cc1f10b7a98c9db21e345db1'
 		}
 	}
 };
--- webserver/app.js
+++ webserver/app.js
@@ -46,7 +46,7 @@ app.dynamicHelpers(helpers);
 //-----------------------------------------
 // Start server
 //-----------------------------------------
-var port = parseInt(process.argv[2], 10) || 3000;
+var port = process.env.PORT || parseInt(process.argv[2], 10) || 4000;
 app.listen(port);
 if (app.address())
 	console.log("watchmen server listening on port %d in %s mode", app.address().port, app.settings.env);
