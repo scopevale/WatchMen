a3d4a5d090aa928d1028c93e96f475624817f906
scopevale <scopevale@gmail.com>
2012-11-15 22:51:57 +0000

    corrected postmark from address - needed a certified address - see postmark account

--- config/general.js
+++ config/general.js
@@ -1,10 +1,10 @@
 module.exports = {
     'notifications' : {
 		enabled: true, //if disabled, no notifications will be sent
-//		to: ['gary.smith@workplacesystems.com'], //default notification list if no alert_to is specified for host or url
-    	to: ['scopevale.1@gmail.com'], //default notification list if no alert_to is specified for host or url
+		to: ['gary.smith@workplacesystems.com'], //default notification list if no alert_to is specified for host or url
+    	cc: ['scopevale.1@gmail.com'], //default notification list if no alert_to is specified for host or url
 		postmark : {
-			from: 'garys@wpmail.com',
+			from: 'gary.smith@wpmail.com',
 			api_key : '016b3fbc-f238-441c-ae30-027570f7ad60'
 		}
 	}
