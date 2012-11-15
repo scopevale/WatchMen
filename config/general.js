edaa71e879bd1873988827e54d04f9284a4dc986
Gary Smith <scopevale@gmail.com>
2012-11-15 17:42:14 -0500

    enabled postmark hook

--- config/general.js
+++ config/general.js
@@ -1,7 +1,8 @@
 module.exports = {
     'notifications' : {
-		enabled: false, //if disabled, no notifications will be sent
-		to: ['gary.smith@workplacesystems.com'], //default notification list if no alert_to is specified for host or url
+		enabled: true, //if disabled, no notifications will be sent
+//		to: ['gary.smith@workplacesystems.com'], //default notification list if no alert_to is specified for host or url
+    	to: ['scopevale.1@gmail.com'], //default notification list if no alert_to is specified for host or url
 		postmark : {
 			from: 'garys@wpmail.com',
 			api_key : '016b3fbc-f238-441c-ae30-027570f7ad60'
